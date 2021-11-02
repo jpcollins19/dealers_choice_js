ec7c4b1c3973c3da093291d46a4c94db361b4ad9
MisterDevDev <58986792+MisterDevDev@users.noreply.github.com>
2021-11-02 14:12:01 -0400

    Update main.js
    feedback comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Not the most robust of dealer's choice submissions I've received, but it gets the job done
 const express = require(`express`);
 const morgan = require(`morgan`);
 const actorDetails = require(`./actorDetails`);
@@ -8,6 +9,7 @@ const actorInfo = require(`./html/actor-info`);
 const app = express();
 
 app.use(require(`morgan`)(`dev`));
+//I think it's cleaner to deliver one static folder with css and images, but that's preference
 app.use(`/css`, express.static(path.join(__dirname, `css`)));
 app.use(`/images`, express.static(path.join(__dirname, `images`)));
 
@@ -20,9 +22,11 @@ app.get(`/`, (req, res, next) => {
 app.get(`/details/:id`, (req, res, next) => {
   const detail = actorDetails.find(req.params.id);
 
+// I'd either export an anonymous function in your html file or change the require name in this file. This code is telling me that actorInfo has more properties
   res.send(actorInfo.actorInfo(detail));
 });
 
+//I would get into the habit of declaring the environment port so your apps can deploy with issue
 const port = 2000;
 
 app.listen(port, () => {
